b3765de09a6675cbee54b16f0980a4d0aef1db2b
Chris Howarth <chowarth23@gmail.com>
2020-12-12 21:38:35 +0000

    Add page constant attribute

---
+++ src/Prism.Generators.PageConstants/PageConstantAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Prism.Generators.PageConstants
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public sealed class PageConstantAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public PageConstantAttribute(string name = default)
+            => Name = name;
+    }
+}
